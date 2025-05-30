3c2af923acf86891a9835fbd52557a60faa4d874
Ray Yuan Liu <louis383@gmail.com>
2025-05-30 21:02:30 +0800

    Remove test

--- src/test/kotlin/com/ivyapps/composehammer/util/AssertionsUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.ivyapps.composehammer.util
-
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
-
-infix fun <T> T.shouldBe(expected: T) {
-    BasePlatformTestCase.assertEquals(expected, this)
-}
